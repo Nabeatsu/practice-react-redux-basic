995b36ad6a2b72fa767f39b05699081d7339e301
Nobuyuki Tanabe <tanabe.1478@gmail.com>
2019-03-24 17:02:59 +0900

    Provider と Connect / store を React で使用

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+import React from "react";
+import { render } from "react-dom";
+
 import { createStore } from "redux";
 import reducer from "./reducer";
 
-// store の作成
+// React Component に store を渡すために
+// Provider でラッピング
+import { Provider } from "react-redux";
+
+// 別ファイルに切り脱している
+import App from "./App";
+
 const store = createStore(reducer);
 
-// store が変更された際に実行する内容を登録する
-store.subscribe(() => {
-  // store.getState() で state を取得できる
-  console.log(store.getState());
-});
-
-// action を dispatch
-store.dispatch({ type: "PLUS", payload: { num: 1 } });
-store.dispatch({ type: "PLUS", payload: { num: 10 } });
-store.dispatch({ type: "MINUS", payload: { num: 1 } });
-store.dispatch({ type: "MINUS", payload: { num: 10 } });
+// Provider でラップした上で
+// store を渡す
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
